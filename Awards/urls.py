29777c49c796960eee3eb64ddebaa813d4e73c24
skimanikings <kimanisimon856@gmail.com>
2020-02-16 18:43:12 +0300

    configure the static files in the settings

--- Awards/urls.py
+++ Awards/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.conf.urls import url,include
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
+from django.conf.urls.static import static
+from django.conf import settings
 
 
 urlpatterns = [
@@ -24,3 +26,5 @@ urlpatterns = [
     url(r'login/',auth_views.LoginView.as_view(template_name="login.html"), name="login"),
     
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
