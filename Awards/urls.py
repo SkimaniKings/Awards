9bc6750342fcd80915cbf187731f7ddf62914be3
skimanikings <kimanisimon856@gmail.com>
2020-02-16 18:21:19 +0300

    create a function that will return the registration template

--- Awards/urls.py
+++ Awards/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
 """
 from django.conf.urls import url,include
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
+
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'',include("ratings.urls"))
+    url(r'',include("ratings.urls")),
+    url(r'login/',auth_views.LoginView.as_view(template_name="login.html"), name="login"),
+    
 ]
