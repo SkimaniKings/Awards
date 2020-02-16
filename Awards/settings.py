d7193645d8af103034174a0360d67d2cb1050fb4
skimanikings <kimanisimon856@gmail.com>
2020-02-16 18:36:18 +0300

    configure the crispy forms in the settings

--- Awards/settings.py
+++ Awards/settings.py
@@ -32,6 +32,8 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'ratings.apps.RatingsConfig',
+    'crispy_forms',
+    'crispy_forms',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
