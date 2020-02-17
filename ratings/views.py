06e0cd4c8ac4bc5a08e0da6da787d4de23031f47
skimanikings <kimanisimon856@gmail.com>
2020-02-17 07:34:08 +0300

    make migrations and import redirect module and succesfully create an account

--- ratings/views.py
+++ ratings/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .forms import UserReagisterForm
+from django.contrib import messages
+
 
 
 # Create your views here.
