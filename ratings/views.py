4011b86022ecb4c4a5b54de53bc958cb04009601
skimanikings <kimanisimon856@gmail.com>
2020-02-17 07:14:14 +0300

    create a forms.py file

--- ratings/views.py
+++ ratings/views.py
@@ -1,9 +1,19 @@
 from django.shortcuts import render
 
 # Create your views here.
-
-def index(request):
-    return render(request, "index.html")
+def index(request)
+    form=UserReagisterForm()
+    if request.method == "POST":
+        form=UserReagisterForm(request.POST)
+        if form.is_valid():
+                
+            form.save()
+            username = form.cleaned_data.get("username")
+            messages.success(request, ("Account for {} has been created successfully").format(username))
+            return redirect("login")
+    else:
+        form=UserReagisterForm()
+    return render(request, "index.html",{"form":form})
 
 
 def register(request):
