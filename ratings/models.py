48e01e06ab5625b300e68f7cfba49e7ec3738bab
skimanikings <kimanisimon856@gmail.com>
2020-02-19 09:42:41 +0300

    created a project model

--- ratings/models.py
+++ ratings/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+import datetime as dt
+
 
 
 # Create your models here.
@@ -8,4 +10,19 @@ class Profile(models.Model):
       
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     image = models.ImageField(default="roman_reigns.jpg")
+    
+    
+class Project(models.Model):
+    title = models.CharField(max_length=50)
+    image = models.ImageField()
+    description = models.CharField(max_length=200)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    date_posted = models.DateTimeField(auto_now=True)
+    link = models.URLField(max_length=250)
+    country = models.CharField(max_length=50)
+
+    
+
+    def __str__(self):
+        return self.title
   
