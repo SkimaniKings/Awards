3d3cef16c43d269f06e017465b14de4c8efc89eb
skimanikings <kimanisimon856@gmail.com>
2020-02-20 17:47:53 +0300

    made deployment changes

--- ratings/urls.py
+++ ratings/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    url(r'', views.index, name='index'),
     url('register/', views.register, name='register'),
     url('submit/', views.submit, name='submit'),
     url('profile/', views.profile, name='profile'),
