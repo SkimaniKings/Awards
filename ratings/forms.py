1bfd8a48b28ef2b3f3bf4d722b0db95db66fc763
skimanikings <kimanisimon856@gmail.com>
2020-02-20 10:33:59 +0300

    updated the information on the home page to be displayed in form of cards

--- ratings/forms.py
+++ ratings/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from .models import Project
 
 
 
@@ -10,3 +11,10 @@ class UserReagisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ["username", "email", "password1", "password2"]
+        
+        
+class PostUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Project
+        fields = ["title","image","user","description","link"]
+    
